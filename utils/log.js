0c43196de41935f2e09058d7768ad1644824207c
ga-avery <averyhunterwood5x@gmail.com>
2021-05-18 09:52:12 -0700

    add: command line flags for node style imports add: missing packages add: helpers

---
+++ utils/log.js
@@ -0,0 +1,28 @@
+import colors from 'colors';
+
+const log = {
+  /**
+   * Wraps the logged items with green checkboxes e.g. [✓] args [✓]
+   * @param  {...any} args Statements that you want to be printed in the console
+   * @returns 
+   */
+  success: (...args) => console.log(colors.green('[✓]'), ...args, colors.green('[✓]')),
+  /**
+   * Wraps the logged items with yellow alertboxes e.g. [!] args [!]
+   * @param  {...any} args 
+   * @returns 
+   */
+  caution: (...args) => console.log(colors.yellow('[!'), ...args, colors.yellow('[!')),
+  /**
+   * Wraps the error with red x boxes with each box on newlines
+   * e.g.
+   * [𝑥]
+   * args
+   * [𝑥]
+   * @param  {...any} args 
+   * @returns 
+   */
+  error: (...args) => console.log(colors.red('[𝑥]\n'), ...args, colors.red('\n[𝑥]')),
+}
+
+export default log
